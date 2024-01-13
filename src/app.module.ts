27311d17e5d7e55ce11305e89822890cd968320d
lucius zhao <nick_zzhao@outlook.com>
2024-01-13 17:54:15 +0800

    50 end

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,20 +4,29 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from './config/app.config';
 
 @Module({
   imports: [
-    CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
-      autoLoadEntities: true,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        return {
+          type: 'postgres',
+          host: process.env.DATABASE_HOST,
+          port: +process.env.DATABASE_PORT,
+          username: process.env.DATABASE_USER,
+          password: process.env.DATABASE_PASSWORD,
+          database: process.env.DATABASE,
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
     }),
+    ConfigModule.forRoot({
+      load: [appConfig],
+    }),
+    CoffeesModule,
     CoffeeRatingModule,
   ],
   controllers: [AppController],
