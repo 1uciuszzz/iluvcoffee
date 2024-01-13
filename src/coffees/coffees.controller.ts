3bfb9aded5f443cf5b3f35a91f316cbc266ed2fd
lucius zhao <nick_zzhao@outlook.com>
2024-01-13 09:54:58 +0800

    25 end

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -21,7 +21,7 @@ export class CoffeesController {
   }
 
   @Get(':id')
-  find_one(@Param('id') id: string) {
+  find_one(@Param('id') id: number) {
     return this.coffeesService.findOne(id);
   }
 
@@ -31,12 +31,12 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+  update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
     return this.coffeesService.update(id, updateCoffeeDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
+  remove(@Param('id') id: number) {
     return this.coffeesService.remove(id);
   }
 }
